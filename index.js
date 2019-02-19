8c7f588ca362a9dd10eb8a1d23d67e95f08bbdb4
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-02-19 16:39:44 -0600

    MVP and Strech Finished

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const knex = require('knex');
 const knexConfig = {
   client: "sqlite3",
   connection: {
-    filename: '/data/lambda.sqlite3'
+    filename: './data/lambda.sqlite3',
   },
   useNullAsDefault: true, // you need this for sqlite3
 }
@@ -19,19 +19,154 @@ server.use(helmet());
 
 // endpoints here
 
-// list all zoos
-server.get('/api/zoos', (req, res) => {
-// get all zoos from the database
+// POST
+server.post('/api/zoos', async (req, res) => {
+  try {
+    const [id] = await db('zoos').insert(req.body);
 
+    const zoo = await db('zoos')
+      .where({ id })
+      .first();
+
+    res.status(201).json(zoo);
+  } catch (error) {
+    const message = errors[error.errno] || 'There was an error posting this zoo';
+    res.status(500).json({ message, error });
+  }
 });
 
-// list zoos by id
+// GET all zoos from the database
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-// create new zoo
+// GET all zoos by ID
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+    const zoo = await db('zoos')
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(zoo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // update zoo
+server.put('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .update(req.body);
 
-// remove a zoo 
+    if (count > 0) {
+      const zoo = await db('zoos')
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({ message: 'Zoo information not found' });
+    }
+  } catch (error) {}
+});
+
+// DELETE a zoo 
+server.delete('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Zoo information not found' });
+    }
+  } catch (error) {}
+});
+
+
+// BEARS
+
+server.post('/api/bears', async (req, res) => {
+  try {
+    const [id] = await db('bears').insert(req.body);
+
+    const bear = await db('bears')
+      .where({ id })
+      .first();
+
+    res.status(201).json(bear);
+  } catch (error) {
+    const message = errors[error.errno] || 'There was an error posting this bear';
+    res.status(500).json({ message, error });
+  }
+});
+
+// GET all bears from the database
+server.get('/api/bears', async (req, res) => {
+  try {
+    const bears = await db('bears');
+    res.status(200).json(bears);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET all bears by ID
+server.get('/api/bears/:id', async (req, res) => {
+  try {
+    const bear = await db('bears')
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(bear);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// update a bear
+server.put('/api/bears/:id', async (req, res) => {
+  try {
+    const count = await db('bears')
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const bear = await db('bears')
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(bear);
+    } else {
+      res.status(404).json({ message: 'Zoo information not found' });
+    }
+  } catch (error) {}
+});
+
+// DELETE a bear
+server.delete('/api/bears/:id', async (req, res) => {
+  try {
+    const count = await db('bears')
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Bear information not found' });
+    }
+  } catch (error) {}
+});
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
